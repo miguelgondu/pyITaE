7e5df143588beb47024bae3073c7ff99d8eeda3d
Miguel Gonzalez Duque <migd@itu.dk>
2019-11-21 14:42:54 +0100

    Initial commit

---
+++ readme.md
@@ -0,0 +1,4 @@
+This package contains an implementation of Intelligent Trial and Error, inspired on the original pyite package developed by JB Mouret and his lab.
+
+TODO:
+    - add links and documentation
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="pyITaE",
+    author="Miguel González Duque",
+    description="An implementation of the Intelligent Trial and Error algorithm",
+    license="MIT",
+    packages=["pyITaE"]
+)
