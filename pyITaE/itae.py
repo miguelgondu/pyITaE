2489c78501e26432ade004dd27715ef10fca4eb5
Miguel Gonzalez Duque <migd@itu.dk>
2019-11-27 16:50:59 +0100

    Starts implementing the deploy method inspired in pyite

--- pyITaE/itae.py
+++ pyITaE/itae.py
@@ -4,7 +4,11 @@ of an ITaE application.
 
 TODO: test everything.
 '''
+import numpy as np
 import json
+import GPy
+# TODO: add different kernels than RBF.
+from sklearn.gaussian_process import GaussianProcessRegressor
 from pymelites.map_elites import Cell, MAP_Elites
 
 class ITaE:
@@ -14,25 +18,42 @@ class ITaE:
 
     It takes:
         - The ingredients for the map creation process using MAP_Elites from pymelites.
+            - simulate
+            - acquisition
             - random_solution
             - random_selection
             - random_variation
-            - simulate
+          Notice that the simulate is first, and acquisition is second.
     It creates empty versions of
         - The partition that describes the map.
         - a map_elites attribute that stores the MAP_Elites object when
           creating the map from scratch.
         - cells and solutions, which essentially constitute the map.
+    
+    TODO:
+        - Implement get_next_best_genotype using the acquisition function.
+        - Test the compute_map method.
+        - Load behaviors and store them in load_map.
+        - Add a run kwarg, with the deployment function.
     '''
-    def __init__(self, random_solution, random_selection, random_variation, simulate):
+    def __init__(self, simulate=None, acquisition=None, random_solution=None, random_selection=None, random_variation=None):
+        if simulate is None:
+            raise ValueError("The simulate argument is required and mustn't be None.")
+
+        if acquisition is None:
+            raise ValueError("The acquisition argument is required and mustn't be None.")
+
+        self.simulate = simulate
+        self.acquisition = acquisition
+
         self.random_solution = random_solution
         self.random_selection = random_selection
         self.random_variation = random_variation
-        self.simulate = simulate
         self.partition = None
         self.map_elites = None
         self.cells = None
         self.solutions = None
+        self.behaviors = None
 
     def compute_map(self, partition, generations, iterations_per_gen, generation_path='.'):
         '''
@@ -45,6 +66,16 @@ class ITaE:
             - test this.
         '''
         # Create the MAP_Elites object, create the cells, and compute the archive
+
+        if self.random_solution is None:
+            raise ValueError("random_solution must be a function, not None.")
+
+        if self.random_selection is None:
+            raise ValueError("random_selection must be a function, not None.")
+
+        if self.random_variation is None:
+            raise ValueError("random_variation must be a function, not None.")
+
         self.map_elites = MAP_Elites(
             self.random_solution,
             self.random_selection,
@@ -78,9 +109,56 @@ class ITaE:
         with open(path) as fp:
             doc = json.load(fp)
 
+        # TODO: load behaviors as well
         self.cells = {}
         self.solutions = {}
         for cell in doc.values():
             centroid = tuple(cell['centroid'])
             self.cells[centroid] = Cell.from_dict(cell)
             self.solutions[centroid] = cell['solution']
+        
+    
+    def deploy(self):
+        # Initialization
+        rho = 0.4
+        variance_noise_square = 0.001
+        tested_behaviors = []
+        Y = [] # the difference between real performance and map performance.
+
+        next_best_genotype, map_performance = self.get_next_best_genotype()
+
+        # Loop
+        while True:
+            # This shouldn't be simulate, but run. A new function in deployment.
+            behavior, real_performance = self.simulate(next_best_genotype)
+            
+            # Update the domain and codomain of the GP.
+            if len(tested_behaviors) == 0:
+                tested_behaviors = np.array(list(behavior))
+            else:
+                tested_behaviors = np.vstack((tested_behaviors, behavior))
+            
+            y = real_performance - map_performance
+            if len(Y) == 0:
+                Y = np.array([y])
+            else:
+                Y = np.vstack((Y, np.array([y])))
+            
+            # Compute the GP with the updated domain and codomain.
+            dimension = behavior.shape[0]
+            ker = GPy.kern.Matern52(dimension,lengthscale = rho, ARD=False) + GPy.kern.White(dimension,np.sqrt(variance_noise_square))
+            m = GPy.models.GPRegression(tested_behaviors, Y, ker)
+
+            # Predict the map "real - simulation"
+            # TODO: fill behaviors.
+            means, variances = m.predict(self.behaviors)
+
+            # From this, get the map "real" by adding the simulation
+            self.pred_performances = means + self.map_performances
+
+            next_best_genotype, map_performance = self.get_next_best_genotype()
+
+            # TODO: add an exit condition.
+
+            
+
