84637471a982170a040afa18bc5fd296febf868d
Miguel Gonzalez Duque <migd@itu.dk>
2019-12-14 09:07:54 +0100

    Pre trip update

--- pyITaE/itae.py
+++ pyITaE/itae.py
@@ -7,7 +7,6 @@ TODO: test everything.
 import numpy as np
 import json
 import GPy
-# TODO: add different kernels than RBF.
 from sklearn.gaussian_process import GaussianProcessRegressor
 from pymelites.map_elites import Cell, MAP_Elites
 
@@ -34,18 +33,23 @@ class ITaE:
         - Implement get_next_best_genotype using the acquisition function.
         - Test the compute_map method.
         - Load behaviors and store them in load_map.
-        - Add a run kwarg, with the deployment function.
     '''
-    def __init__(self, simulate=None, acquisition=None, random_solution=None, random_selection=None, random_variation=None):
+    def __init__(self, simulate=None, acquisition=None, run=None, random_solution=None, random_selection=None, random_variation=None):
         if simulate is None:
             raise ValueError("The simulate argument is required and mustn't be None.")
 
         if acquisition is None:
             raise ValueError("The acquisition argument is required and mustn't be None.")
 
+        if run is None:
+            raise ValueError("The run argument is required and mustn't be None.")
+
+        # Required fields
         self.simulate = simulate
         self.acquisition = acquisition
+        self.run = run
 
+        # Others (for MAP-Elites running, for example)
         self.random_solution = random_solution
         self.random_selection = random_selection
         self.random_variation = random_variation
@@ -54,6 +58,7 @@ class ITaE:
         self.cells = None
         self.solutions = None
         self.behaviors = None
+        self.map_performances = None
 
     def compute_map(self, partition, generations, iterations_per_gen, generation_path='.'):
         '''
@@ -116,37 +121,168 @@ class ITaE:
             centroid = tuple(cell['centroid'])
             self.cells[centroid] = Cell.from_dict(cell)
             self.solutions[centroid] = cell['solution']
+            self.map_performances[centroid] = cell['performance']
+
+    def get_next_genotype(self, init=False, kappa=0.1):
+        # GP = []
+        # for i in range(0,len(mu_map)):
+        #     GP.append(mu_map[i] + kappa*sigma_map[i])
+        # return np.argmax(GP)
+        if not init:
+            # This won't work, because I'm storing everything in dicts.
+            # TODO: fix this.
+
+            return np.argmax(self.real_map + kappa*self.variance)
+        if init:
+            best_performance = -np.Inf
+            best_centroid = None
+            for centroid, performance in self.performances: # TODO: implement genotype getting.
+                if performance >= best_performance:
+                    best_centroid = centroid
+
+            return self.genotypes[best_centroid]
+            
         
-    
+    def deploy2(self, run):
+        '''
+        This function takes a run procedure (a function similar to that of
+        self.simulation, which takes a genotype and runs it, getting a behavior
+        descriptor and a real performance).
+
+        I should maybe maintain a "real_performance" dict that starts as the "map_performance" but is updated at each iteration. Now that I think about it, it doesn't make sense to build a new thing. This is just the mean of the GP.
+
+        STEPS IN THE ORIGINAL ITE:
+        1. Load the map
+        2. Filter it and create numpy arrays
+        3. Create a copy of the performance map.
+            - one as the "real" map, another as a saved copy for adding to the mean.
+            - They don't really use the n_fits_real for anything. They overwrite it with mean + map from the get-go.
+        4. Get the next "index" to test, which is essentially
+           getting the next "controller", or genotype in my
+           notation
+        5. Initialize the X and Y of the GP.
+        6. In the while loop:
+            6.1. Define the kernel of the GP (this could be done outside I would argue.)
+            6.2. predict the value of the mean in the 
+            low-dimensional behavior space (i.e. at the 
+            description space.)
+            6.3. use this mean to compute the actual real values.
+            6.4. Compute the percentages of changed values.
+            6.5. Get the next index to test.
+            6.6. If this new index has already been tested,
+                 register that, prepare to test said index again
+                 and up the counter for repetitions (?)
+
+                 They don't actually test the index again.
+            6.7. If this new index hasn't been tested, simulate
+                 it and record performance and behavior.
+            6.8. Update X and Y for the GP with relevant
+                 values.
+            6.9. Maintain the lists of real performances,
+                 tested controllers (genotypes), tested
+                 descriptions (behaviors), the number of
+                 iterations.
+
+                 Things like the real performance list
+                 are only being kept for archiving, they
+                 aren't used in the code itself as far as I
+                 can tell.
+            6.10. Check the stopping condition. Stop if
+                  that's the case.
+        
+
+        TODO: finish this documentation.
+        '''
+        # Init
+        # TODO: get back to this.
+        running = True
+        tested_behaviors = None
+        Y = None
+        while running:
+            # Main loop.
+            # Get and run the best candidate genotype.
+            next_genotype, map_performance = self.get_next_genotype()
+            behavior, real_performance = run(next_genotype)
+
+            # Update the X and Y of the GP.
+            x = np.array([behavior])
+            y = np.array([real_performance - map_performance])
+            if tested_behaviors is None:
+                tested_behaviors = x
+            else:
+                tested_behaviors = np.vstack((tested_behaviors, x))
+            
+            if Y is None:
+                Y = y
+            else:
+                Y = np.vstack(Y, y)
+
+            # Compute and update the GP
+            dimension = behavior.shape[0]
+            ker = GPy.kern.Matern52(dimension, lengthscale=1, ARD=False) + GPy.kern.White(dimension,np.sqrt(0.1))
+            m = GPy.models.GPRegression(tested_behaviors, Y, ker)
+
+            # Return the map to normal by predicting and adding.
+            # means = []
+            variances = []
+            real_map = {}
+            for centroid, behavior in self.behaviors.items():
+                mean, variance = m.predict(np.array([behavior]))
+                # means.append(mean)
+                variances.append(variance)
+                # Remember: mean is real - map performance.
+                real_map[centroid] = mean + self.map_performances[centroid]
+
+            # This is a very dumb way of doing this, I wonder if I could improve
+            # it or just move to storing the raw np arrays as they do.
+            # I now think it makes sense to stick with
+            # dicts, and to do it this way, because
+            # the optimization is continuous, and
+            # updating the map by having memory of pre-
+            # vious maps doesn't make sense.
+            self.real_map = real_map
+            means, variances = np.array(means), np.array(variances)
+
+            # decide if we want to stop.
+            # TODO: implement the exit condition.
+            exit_condition = False
+            if exit_condition:
+                running = False
+
+
     def deploy(self):
-        # Initialization
+        # Initialization.
         rho = 0.4
         variance_noise_square = 0.001
         tested_behaviors = []
+        tested_genotypes = {}
         Y = [] # the difference between real performance and map performance.
 
         next_best_genotype, map_performance = self.get_next_best_genotype()
 
         # Loop
         while True:
-            # This shouldn't be simulate, but run. A new function in deployment.
-            behavior, real_performance = self.simulate(next_best_genotype)
-            
-            # Update the domain and codomain of the GP.
+            # Run the next genotype, if we haven't tested it before (TODO: add prior testing verification)
+            behavior, real_performance = self.run(next_best_genotype)
+            tested_genotypes[next_best_genotype] = real_performance
+
+            # Update the domain and codomain of the GP. GP is modeling
+            # the difference between the real performance and the
+            # map's performance.
             if len(tested_behaviors) == 0:
                 tested_behaviors = np.array(list(behavior))
             else:
                 tested_behaviors = np.vstack((tested_behaviors, behavior))
-            
+
             y = real_performance - map_performance
             if len(Y) == 0:
                 Y = np.array([y])
             else:
                 Y = np.vstack((Y, np.array([y])))
-            
+
             # Compute the GP with the updated domain and codomain.
             dimension = behavior.shape[0]
-            ker = GPy.kern.Matern52(dimension,lengthscale = rho, ARD=False) + GPy.kern.White(dimension,np.sqrt(variance_noise_square))
+            ker = GPy.kern.Matern52(dimension, lengthscale=rho, ARD=False) + GPy.kern.White(dimension,np.sqrt(variance_noise_square))
             m = GPy.models.GPRegression(tested_behaviors, Y, ker)
 
             # Predict the map "real - simulation"
@@ -156,9 +292,9 @@ class ITaE:
             # From this, get the map "real" by adding the simulation
             self.pred_performances = means + self.map_performances
 
+            # Huh, the meaning of map here is different from above. It should be querying
+            # the updated map.
             next_best_genotype, map_performance = self.get_next_best_genotype()
 
             # TODO: add an exit condition.
-
-            
 
---
+++ pyITaE/itae_take_2.py
@@ -0,0 +1,210 @@
+#! /usr/bin/env python
+#| This file is a part of the pyite framework.
+#| Copyright 2019, INRIA
+#| Main contributor(s):
+#| Jean-Baptiste Mouret, jean-baptiste.mouret@inria.fr
+#| Eloise Dalin , eloise.dalin@inria.fr
+#| Pierre Desreumaux , pierre.desreumaux@inria.fr
+#|
+#| Antoine Cully, Jeff Clune, Danesh Tarapore, and Jean-Baptiste Mouret.
+#|"Robots that can adapt like animals." Nature 521, no. 7553 (2015): 503-507.
+#|
+#| This software is governed by the CeCILL license under French law
+#| and abiding by the rules of distribution of free software.  You
+#| can use, modify and/ or redistribute the software under the terms
+#| of the CeCILL license as circulated by CEA, CNRS and INRIA at the
+#| following URL "http://www.cecill.info".
+#|
+#| As a counterpart to the access to the source code and rights to
+#| copy, modify and redistribute granted by the license, users are
+#| provided only with a limited warranty and the software's author,
+#| the holder of the economic rights, and the successive licensors
+#| have only limited liability.
+#|
+#| In this respect, the user's attention is drawn to the risks
+#| associated with loading, using, modifying and/or developing or
+#| reproducing the software by the user in light of its specific
+#| status of free software, that may mean that it is complicated to
+#| manipulate, and that also therefore means that it is reserved for
+#| developers and experienced professionals having in-depth computer
+#| knowledge. Users are therefore encouraged to load and test the
+#| software's suitability as regards their requirements in conditions
+#| enabling the security of their systems and/or data to be ensured
+#| and, more generally, to use and operate it in the same conditions
+#| as regards security.
+#|
+#| The fact that you are presently reading this means that you have
+#| had knowledge of the CeCILL license and that you accept its terms.
+from ite_utils import *
+import sys
+import time
+sys.path.append('..')
+
+class ITE:
+    def __init__(self):
+        pass
+
+def ite(params):
+	# Load the map
+	centroids = load_centroids(sys.argv[1])
+	fits, descs, ctrls = load_data(sys.argv[2], centroids.shape[1],dim_ctrl)
+
+	# Filtering, which doesn't actually take place
+	n_fits, n_descs, n_ctrls = [], [], []
+	for i in range(0,len(fits)):
+		#if(fits[i]>1): #filter the fitness , discard the ones < 1
+		if(1):
+			n_fits.append(fits[i])
+			n_descs.append(descs[i])
+			n_ctrls.append(ctrls[i])
+	n_fits = np.array(n_fits)
+	n_descs = np.array(n_descs)
+	n_ctrls = np.array(n_ctrls)
+
+	# MIGUEL:
+	# the n_whatever are essentially copies of the original fits but as numpy arrays.
+
+	# Huh, they copy the "performance map" twice.
+	# do they modify n_fits?, what's the need for copying twice.
+	# They don't. Funny. They work with n_fits_real
+	# and fits_saved from here on.
+	n_fits_real = copy(np.array(n_fits))
+	fits_saved = copy(n_fits)
+	next_index_to_test = np.argmax(n_fits) # The first cell to test is the best fitness, yeah.
+
+	################ INIT ITE  ###########################################################
+	robot_utils.end_episode_distance = -1000 
+	prev_ep_distance = -1000
+
+	init, run = True, True
+	num_it = 0
+	# QUESTION: why do they start the real preferences like this?, just as a "step -1" or something?
+	# ANSWER: yes, this is an initialization, as it says above.
+	# Q: why do they instantiate real_perfs here if they overwrite it on the init step.
+	real_perfs, tested_indexes, tested_ctrls, tested_descs =[-1000], [], [], []
+	X, Y = [], []
+	############## BEGIN ITE ###################################################
+	while(run):
+		stop_cond =  alpha*max(n_fits_real)
+
+		if(not init):
+			#define GP kernerl
+			ker = GPy.kern.Matern52(dim_x,lengthscale = rho, ARD=False) + GPy.kern.White(dim_x,np.sqrt(variance_noise_square))
+			#define Gp which is here the difference btwn map perf and real perf
+			m = GPy.models.GPRegression(X,Y,ker)
+			#predict means and variances for the difference btwn map perf and real perf
+			means, variances = m.predict(n_descs)
+			#Add the predicted difference to the map found in simulation
+			# They overwrite the whole n_fits_real!,
+			for j in range(0,len(n_fits_real)):
+				n_fits_real[j] = means[j] + fits_saved[j]
+			#Compute how many percent of the behaviors have been significantly changed by this iteration
+			changed = 0
+			if(len(means_prev)>0):
+				for y in range(0,len(means)):
+					if(abs(means[y]-means_prev[y])>0.1):
+						changed = changed + 1
+				percent = changed*100/len(means)
+				percents.append(percent)
+			means_prev = means
+			#apply acquisition function to get next index to test
+			next_index_to_test = UCB(n_fits_real,kappa,variances)
+		else:
+			real_perfs = [] # Q: Why do they overwrite the [-1000].
+			init = False
+		#if the behavior to test has already been tested, don't test it again
+		if(next_index_to_test in tested_indexes):
+			# What's happening here?!, an np.index wouldn't suffice?
+			for h in range(0,len(tested_indexes)):
+				if(next_index_to_test==tested_indexes[h]):
+					tmp_id = h
+			real_perf = real_perfs[tmp_id]
+			ctrl_to_test = n_ctrls[next_index_to_test]
+			tested_indexes.append(next_index_to_test)
+			in_count = in_count + 1
+		else:
+			# Run the simulation and append the index
+			# to the set of tested indexes.
+			ctrl_to_test = n_ctrls[next_index_to_test]
+			tested_indexes.append(next_index_to_test)
+			# eval the real performance
+			if(ros==1):
+				eval_ros(episode_duration, ctrl_to_test, rate,ros)
+				if(robot_utils.end_episode_distance == prev_ep_distance):
+					sys.exit("WARNING : ite hasn't been able to recover the latest distance, exiting ite")
+				else:
+					prev_ep_distance = robot_utils.end_episode_distance
+					real_perf = robot_utils.end_episode_distance
+			else:
+				real_perf, real_desc = eval_sim(ctrl_to_test, gui=params["gui"])
+		if(len(X)==0):
+			X.append(n_descs[next_index_to_test])
+			Y.append(np.array(real_perf) - fits_saved[next_index_to_test]) # THIS is the important part.
+			X = np.array(X)
+			Y = np.array(Y)
+		else:
+			# Else they just stack (maintain) the vector, add more stuff, like Danny does.
+			X = np.vstack((X, n_descs[next_index_to_test]))
+			Y = np.vstack((Y, np.array(real_perf)-fits_saved[next_index_to_test]))
+		
+		# store it
+		real_perfs.append(real_perf)
+		tested_ctrls.append(ctrl_to_test)
+		# store the tested descs
+		tested_descs.append(n_descs[next_index_to_test])
+		o = np.argmax(real_perfs)
+		max_index = tested_indexes[o]
+		max_perf = real_perfs[o]
+		print("Max performance found with ",num_it," iterations : ", max(real_perfs))
+		print("Associated max index ", max_index)
+		print(real_perfs)
+		print(tested_indexes)
+		num_it = num_it + 1
+		cond = max(real_perfs)
+		if(cond > stop_cond):
+			run = False
+		if(num_it == max_iteration_number):
+			run = False
+
+	print(" ")
+	print("Max performance found with ",num_it," iterations : ", max(real_perfs))
+	print("Associated max index ", max_index)
+	print("Associated controller ",tested_ctrls[np.argmax(real_perfs)])
+	print("Associated descriptor ", tested_descs[np.argmax(real_perfs)])
+	best_ctrls.append(tested_ctrls[np.argmax(real_perfs)])
+	best_descs.append(tested_descs[np.argmax(real_perfs)])
+	num_its.append(num_it)
+	percent_affected_in_map.append(np.mean(np.array(percents)))
+	in_number.append(in_count)
+	mean_final.append(means)
+	var_final.append(variances)
+	best_dist.append(max(real_perfs))
+	best_index.append(max_index)
+	tested_descs_maps.append(tested_descs)
+	tested_ctrls_maps.append(tested_ctrls)
+	tested_indexes_maps.append(tested_indexes)
+	tested_perfs_maps.append(real_perfs)
+	np.save('num_its',np.array(num_its))
+	np.save('percent_affected_in_map',np.array(percent_affected_in_map))
+	np.save('in_number',np.array(in_number))
+	np.save('mean_final',np.array(mean_final))
+	np.save('var_final',np.array(var_final))
+	np.save('best_dist',np.array(best_dist))
+	np.save('best_index',np.array(best_index))
+	np.save('best_ctrls',np.array(best_ctrls))
+	np.save('best_descs',np.array(best_descs))
+	np.save('tested_descs_maps',np.array(tested_descs_maps))
+	np.save('tested_ctrls_maps',np.array(tested_ctrls_maps))
+	np.save('tested_perfs_maps',np.array(tested_perfs_maps))
+	np.save('tested_indexes_maps',np.array(tested_indexes_maps))
+	# plot_GP(mu_map,sigma_map,fits,descs)
+
+params = \
+	    {
+	        "pybullet_minitaur_sim_path": "pybullet_minitaur_sim",
+			"pyhexapod_path": "pyhexapod",
+			"gui" : True,
+	    }
+
+if __name__ == "__main__":
+	ite(params)
